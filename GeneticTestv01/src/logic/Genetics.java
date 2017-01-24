274f98c74bf4fa17711d19f5ffa8cd95ded31c3a
sebaTheWhite <sebastian.przybyl93@gmail.com>
2017-01-24 19:50:11 +0100

    Some changes

--- GeneticTestv01/src/logic/Genetics.java
+++ GeneticTestv01/src/logic/Genetics.java
@@ -1,9 +1,80 @@
 package logic;
 
+import java.util.Iterator;
+import java.util.Set;
+
 public class Genetics {  // utworzyc populacje robic cosokreslona ilosc razy z okreslonym prawdopodobienstwem itp 
 
+	private Set<Set<MyPoint>> firstPopulation;
+	private Set<Set<MyPoint>> nextPopulation;
+	private Set<Set<MyPoint>> presentPopulation;
+	private int replays;
 	
 	
+	Genetics(){
+		MyDistanceTab popul = new MyDistanceTab(10,1000);
+		firstPopulation=popul.getPopulation();
+		
+	}
+
+	public void countNextPopulation(double crossProb){
+		
+		if (crossProb>1.0) return;
+		
+		Set<Set<MyPoint>> presentPopulation=firstPopulation;
+		
+		Iterator<Set<MyPoint>> it = presentPopulation.iterator();
+		
+		double times = ((double)presentPopulation.size()*crossProb);
+		
+		for(int i=0;i<(int)times;i++){ // ile eleentów pobieramy do krzy¿owania
+			
+			Set<MyPoint> temp=it.next();
+			
+			Iterator<MyPoint> itIn = temp.iterator(); // iterowanie po zbiorze podzbioru
+			 tmp=0;
+			
+			while(itIn.hasNext()){
+				tmp=itIn.next().getId_p();
+			
+			}
+		}
+		
+		
+	}
+	
+	public void crossing(double probC){
+		
+		
+		
+	}
+	
+	public void mutating(double probM){
+		
+	}
+	
+	public int getReplays() {
+		return replays;
+	}
+	public void setReplays(int replays) {
+		this.replays = replays;
+	}
+
+	public Set<Set<MyPoint>> getNextPopulation() {
+		return nextPopulation;
+	}
+
+	public void setNextPopulation(Set<Set<MyPoint>> nextPopulation) {
+		this.nextPopulation = nextPopulation;
+	}
+
+	public Set<Set<MyPoint>> getPresentPopulation() {
+		return presentPopulation;
+	}
+
+	public void setPresentPopulation(Set<Set<MyPoint>> presentPopulation) {
+		this.presentPopulation = presentPopulation;
+	}
 	
 	
 	
--- GeneticTestv01/src/logic/MyPoint.java
+++ GeneticTestv01/src/logic/MyPoint.java
@@ -4,10 +4,12 @@ public class MyPoint{
 	private int x;
 	private int y;
 	private static int ID_POINT;
+	private int id_p;
 	
 	public MyPoint(int x,int y){
 		
-		ID_POINT++;
+		setID_POINT(getID_POINT() + 1);
+		id_p=getID_POINT();
 		setX(x);
 		setY(y);
 	}
@@ -30,5 +32,21 @@ public class MyPoint{
 	}
 	public String toString(){
 		return " "+getX()+ " " +getY();
+	}
+
+	public static int getID_POINT() {
+		return ID_POINT;
+	}
+
+	public static void setID_POINT(int iD_POINT) {
+		ID_POINT = iD_POINT;
+	}
+
+	public int getId_p() {
+		return id_p;
+	}
+
+	public void setId_p(int id_p) {
+		this.id_p = id_p;
 	}
 }
